dbd48f83c8c780517aaab7526db8e498fc96085b
hyunsik0213 <albert8545@naver.com>
2026-04-14 20:30:11 +0900

    FEAT : signup_create

---
+++ app/crud/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import hashPassword
+
+INITIAL_SIGNUP_CREDIT = 1000
+
+def getUserByEmail(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def getUserByNickname(db: Session, nickname: str):
+    return db.query(User).filter(User.nickname == nickname).first()
+
+def createUser(db: Session, userIn: UserCreate):
+    hashedPassword = hashPassword(userIn.password)
+    
+    newUser = User(
+        email=userIn.email,
+        nickname=userIn.nickname,
+        password_hash=hashedPassword,
+        credit=INITIAL_SIGNUP_CREDIT
+    )
+    
+    db.add(newUser)
+    db.commit()
+    db.refresh(newUser)
+    
+    return newUser
