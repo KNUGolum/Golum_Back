a60fa84de49be82ea65cd44b789eff4097b8ad38
meatschool <ceojr1234@gmail.com>
2026-04-24 16:07:48 +0900

    FEAT : 웹소켓 엔드포인트 구현 및 라우터 등록

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.endpoints import auth, vote, bet, poll_result, payout
+from app.api.endpoints import auth, vote, bet, poll_result, payout, websocket
 
 apiRouter = APIRouter()
 
@@ -8,3 +8,4 @@ apiRouter.include_router(vote.router, prefix="/poll", tags=["Vote"])
 apiRouter.include_router(bet.router, prefix="/bets", tags=["bet"])
 apiRouter.include_router(poll_result.router, prefix="/poll", tags=["pollResult"])
 apiRouter.include_router(payout.router, prefix="/payout", tags=["payout"])
+apiRouter.include_router(websocket.router, tags=["WebSocket"])
