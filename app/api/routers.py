02005f0534864ba2d02e8b04f8431860e2035a6e
hyunsik0213 <albert8545@naver.com>
2026-04-28 19:45:01 +0900

    Fix routers merge conflict

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,74 +1,26 @@
 from fastapi import APIRouter
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-from app.api.endpoints import auth, poll, vote
-=======
-from app.api.endpoints import auth, vote, bet
->>>>>>> pr-34
-=======
-from app.api.endpoints import auth, vote, bet, poll_result
->>>>>>> pr-35
-=======
-from app.api.endpoints import auth, vote, bet, poll_result, payout, websocket
->>>>>>> pr-36
+from app.api.endpoints import (
+    auth,
+    poll,
+    vote,
+    bet,
+    poll_result,
+    payout,
+    websocket,
+    poll_ratio_update,
+    poll_ratio_read,
+    poll_result_read,
+)
 
 apiRouter = APIRouter()
 
 apiRouter.include_router(auth.router, prefix="/auth", tags=["auth"])
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 apiRouter.include_router(poll.router, prefix="/poll", tags=["poll"])
 apiRouter.include_router(vote.router, prefix="/vote", tags=["vote"])
-=======
-apiRouter.include_router(vote.router, prefix="/poll", tags=["Vote"])
-apiRouter.include_router(bet.router, prefix="/bets", tags=["bet"])
->>>>>>> pr-34
-=======
-apiRouter.include_router(vote.router, prefix="/poll", tags=["Vote"])
-apiRouter.include_router(bet.router, prefix="/bets", tags=["bet"])
-apiRouter.include_router(
-    poll_result.router,  
-    prefix="/poll",      
-    tags=["pollResult"]  
-)
->>>>>>> pr-35
-=======
-apiRouter.include_router(vote.router, prefix="/poll", tags=["Vote"])
 apiRouter.include_router(bet.router, prefix="/bets", tags=["bet"])
 apiRouter.include_router(poll_result.router, prefix="/poll", tags=["pollResult"])
 apiRouter.include_router(payout.router, prefix="/payout", tags=["payout"])
 apiRouter.include_router(websocket.router, tags=["WebSocket"])
->>>>>>> pr-36
-=======
-=======
-from app.api.endpoints import poll_ratio_read
->>>>>>> pr-38
-=======
-from app.api.endpoints import poll_result_read
->>>>>>> pr-39
-#from app.api.endpoints import auth
-# 나중에 polls, bets 라우터도 만들면 여기서 import 합니다.
-# from app.api.endpoints import polls, bets
-from app.api.endpoints import poll_ratio_update
-
-apiRouter = APIRouter()
-
-# 각각의 엔드포인트를 중앙 라우터에 꽂아줍니다.
-#apiRouter.include_router(auth.router, prefix="/auth", tags=["auth"])
-# apiRouter.include_router(polls.router, prefix="/polls", tags=["polls"])
-# apiRouter.include_router(bets.router, prefix="/bets", tags=["bets"])
-<<<<<<< HEAD
-<<<<<<< HEAD
-apiRouter.include_router(poll_ratio_update.router, prefix="/poll", tags=["poll"])
->>>>>>> pr-37
-=======
+apiRouter.include_router(poll_ratio_update.router, prefix="/poll", tags=["pollRatio"])
 apiRouter.include_router(poll_ratio_read.router, prefix="/polls", tags=["polls"])
->>>>>>> pr-38
-=======
 apiRouter.include_router(poll_result_read.router, prefix="/polls", tags=["polls"])
->>>>>>> pr-39
