442636a1eb62aae48041cfd3fe939a0a2c531c87
rldnjs <happy77116563@gmail.com>
2026-05-30 03:40:25 +0900

    FEAT : 칭호 구매 응답 스키마 추가

--- app/schemas/title.py
+++ app/schemas/title.py
@@ -1,14 +1,16 @@
 from pydantic import BaseModel
 from typing import List
 
-
 class ShopTitle(BaseModel):
     titleId: int
     name: str
     grade: str
     price: int
 
-
 class TitleShopListResponse(BaseModel):
     message: str
     titles: List[ShopTitle]
+
+class PurchaseTitleResponse(BaseModel):
+    message: str
+    currentCredit: int
